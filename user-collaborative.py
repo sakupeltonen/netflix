4114460b0d10e985627504dfa511844c4352cb6c
Kuivausrumpusoolo <saku.peltonen@gmail.com>
2023-08-01 13:25:32 +0300

    refactor into preprocess file

--- user-collaborative.py
+++ user-collaborative.py
@@ -3,111 +3,55 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import seaborn as sns
+from preprocess import loadData, filterByRatingCount
 
-df = pd.read_csv('test_data_long.txt', header = None, names = ['customer', 'rating'], usecols = [0,1])
-#df = pd.read_csv('data/combined_data_1.txt', header = None, names = ['customer', 'rating'], usecols = [0,1])
+file = 'test_data_long'
+#file = 'combined_data_1'
 
-"""
-PREPROCESSING
-"""
+df_rating = loadData(file, 'txt')
 
-## df[df['Rating'].isna()] returns a df with the rows where the movie ID appears
-## df[df['Rating'].isna()]['Cust_Id'].str.replace(':', '') removes the : from the movie ID
-## df['Movie_Id'] =    sets a movie ID attribute for those rows
-df['movie'] = df[df['rating'].isna()]['customer'].str.replace(':', '')
+df_rating = filterByRatingCount(df_rating, 30, 200)
 
-## fills the attributes for other rows using the one above
-df['movie'].fillna(method='ffill', inplace=True)
+# print('Fraction of values known: {:.2f}'.format(df_rating.shape[0] / (df_rating['movie'].nunique() * df_rating['customer'].nunique())))
 
-## drops the rows that contained the movie ID originally
-df.dropna(subset=['rating'], inplace=True)
+# =========================================
+#           Initialize tables 
+# =========================================
 
-
-
-
-## number of ratings by customer
-rating_counts = df.groupby(['customer'])['customer'].count()
-rating_counts = rating_counts.sort_values(ascending=False)
-
-selected_users_counts = rating_counts[(rating_counts >= 30) & (rating_counts <= 200)]
-selected_users = selected_users_counts.index
-
-## plot cumulative distribution of rating counts
-# rating_countss = np.bincount(selected_users_counts.values)
-# plt.plot(np.cumsum(rating_countss))
-# plt.show()
+# Create a DataFrame with information for each customer
+means = df_rating.groupby('customer')['rating'].mean()
+counts = df_rating.groupby('customer')['rating'].count()
+df_customers = pd.concat([means, counts], axis=1)
+df_customers.columns = ['average','count']
 
 
-df = df[df['customer'].isin(selected_users)]
+# Create a DataFrame of customers and movies
+table_rating = df_rating.pivot_table(index='customer', columns='movie', values='rating')
+
+# Create a DataFrame of correlations between users
+table_corr = table_rating.T.corr()
+
+table_overlap = (table_rating.notna().astype(int)).dot(table_rating.notna().astype(int).T)
 
 
+# =========================================
+# Predict ratings with collaborative filtering
+# =========================================
 
-
-## --------- BASIC STATISTICS ---------
-print('Fraction of values known: {:.2f}'.format(df.shape[0] / (df['movie'].nunique() * df['customer'].nunique())))
-
-
-
-"""
-Collaborative filtering: predict ranking of movie j for user i
-"""
-user1 = '57633'
-user2 = '786312'
-movie = '2'
-# assert user in df['customer'].values, f'User {user} not in df'
-# assert movie not in df[df['customer']==user]['movie'].values, f'Movie {movie} is already rated by user {user}'
-
-
-""" Compute customer attributes """
-means = df.groupby('customer')['rating'].mean()
-counts = df.groupby('customer')['rating'].count()
-customers = pd.concat([means, counts], axis=1)
-customers.columns = ['average','count']
-
-def pearson(user1, user2):
-    user1movies = df[df['customer']==user1][['movie','rating']].rename(columns={'rating':'rating1'}).set_index('movie')
-    user2movies = df[df['customer']==user2][['movie','rating']].rename(columns={'rating':'rating2'}).set_index('movie')
-    commonMovies = user2movies.merge(user1movies, on='movie')
-    avg1 = customers.loc[user1]['average']
-    avg2 = customers.loc[user2]['average']
-    numerator = ((commonMovies['rating1']-avg1)*(commonMovies['rating2']-avg2)).sum()
-    std1 = math.sqrt(((commonMovies['rating1']-avg1)**2).sum())
-    std2 = math.sqrt(((commonMovies['rating2']-avg2)**2).sum())
-    return numerator / (std1*std2)
-
-
-
-# Create the ratings matrix using pivot_table
-ratings_matrix = df.pivot_table(index='customer', columns='movie', values='rating')
-#movieCorrelations = ratings_matrix.corr()
-correlations = ratings_matrix.T.corr()
-overlap = (ratings_matrix.notna().astype(int)).dot(ratings_matrix.notna().astype(int).T)
-
-print(pearson(user1,user2))
-print(correlations.loc[user1][user2])
-
-# find k closest users for user1
-# k = 5
-# user1corrs = correlations.loc[user1]#.sort_values(ascending=False,na_position='last')
-# usersWithMovieRated = ratings_matrix[movie].notna()
-# usefulCorrs = user1corrs[usersWithMovieRated.values]
-# usefulCorrs = usefulCorrs.sort_values(ascending=False,na_position='last')
-# closeUsers = usefulCorrs.iloc[:k]
-# closeUsersMovieRating = ratings_matrix.loc[closeUsers.index][movie]
-# x = (closeUsersMovieRating-customers.loc[closeUsers.index]['average'])
-# prediction = customers.loc[user1]['average'] + (closeUsers * x).sum() / (closeUsers.abs().sum())
-
-
-def predictRating(user, movie):
-    S = ratings_matrix[ratings_matrix[movie].notna()].index  # users who have rated movie
-    sim = correlations.loc[user]  # correlations between user and others
+def predictRating(user, movie, k=5):
+    S = table_rating[table_rating[movie].notna()].index  # users who have rated movie
+    sim = table_corr.loc[user]  # correlations between user and others
     sim = sim.loc[S]
     sim = sim.sort_values(ascending=False,na_position='last')  # sort descending. not interested in users who don't have movies in common with user
-    k = 5
     sim = sim.iloc[:k]
     Sk = sim.index  # identifiers of users with largest correlation between user
-    r = ratings_matrix.loc[Sk][movie]  # ratings of Sk
-    rc = (r-customers.loc[Sk]['average'])  # ratings of Sk centered around average of Sk
-    prediction = customers.loc[user]['average'] + (sim * rc).sum() / (sim.abs().sum())
+    r = table_rating.loc[Sk][movie]  # ratings of Sk
+    rc = (r-df_customers.loc[Sk]['average'])  # ratings of Sk centered around average of Sk
+    prediction = df_customers.loc[user]['average'] + (sim * rc).sum() / (sim.abs().sum())
     return prediction
 
+
+user1 = 57633
+user2 = 786312
+movie = 2
+predictRating(user1, movie)
